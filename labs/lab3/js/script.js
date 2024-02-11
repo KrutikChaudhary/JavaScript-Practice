6c40c6b7a370892ecb29e5649d7b1d2b7efc804e
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-02-11 17:07:56 -0400

    Corrected minor mistake

--- labs/lab3/js/script.js
+++ labs/lab3/js/script.js
@@ -6,7 +6,12 @@ function displayFormData(){
     for (let i = 0; i < form.elements.length - 1; i++) {
         const element = form.elements[i];
         if (element.type !== 'button') {
-            formIndividualData += `${element.name}: ${element.type === 'radio' ? (element.checked ? 'Yes' : 'No') : element.value}<br>`;
+            if(element.name !== 'gender'){
+                formIndividualData += `${element.name}: ${element.type === 'radio' ? (element.checked ? 'Yes' : 'No') : element.value}<br>`;
+            }
+            else{
+                formIndividualData += `${ (element.checked ?` ${element.name}: ${element.value} <br>`: '')}`;
+            }
         }
     }
 
